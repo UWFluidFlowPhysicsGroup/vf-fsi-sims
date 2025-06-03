44f07d5e535e281f25da7c0158a2ffab969b90cc
nhewkoUW <nhewko@uwaterloo.ca>
2025-06-03 14:26:40 -0400

    Generally improved efficiency/conciseness of code, especially for if statements

--- main.cpp
+++ main.cpp
@@ -24,17 +24,23 @@ const int dim = 3;
 dealii::Tensor<1, dim> fiber;
 
 int main(){
-
-
   //Variables and principal matrix creation
   double E1 = 0, E2 = 0, G12 = 0, nu12 = 0, nu23 = 0;
   //TODO import variable values from parameters, hard coding for now
-  E1 = 131e9; //MPa
-  E2 = 10.3e9; //MPa
+  E1 = 131e9; //GPa
+  E2 = 10.3e9; //GPa
   nu12 = 0.22;
-  nu23 = nu12;
+  nu23 = 0.30;
   G12 = 6.9e9;
   
+  //creating array to get fiber coordinates before creating tensor
+  fiber[0] = 1;
+  fiber[1] = 0;
+  //define z axis only for 3D case (otherwise out of bounds)
+  if (dim == 3){
+    fiber[2] = 2;
+  }
+
   //create elasticity tensor in principal coordinates
   dealii::SymmetricTensor<4, dim> elasticityPrincipal;
   
@@ -46,8 +52,8 @@ int main(){
   const double constk = 1-2*(E2*(1+nu23)*pow(nu12,2))/E1-pow(nu23,2);
 
   //defining G12 same as later on in the for loop, isotropic test case rn
-  //  G12 = (E1*(1-pow(nu23,2))-E2*nu12*(1+nu23))/(2*constk);
-
+  //G12 = (E1*(1-pow(nu23,2))-E2*nu12*(1+nu23))/(2*constk);
+  
   //SymmetricTensor object automatically applies symmetries in ijkl=jikl=ijlk, but still need to manually input ijkl=klij symmetry
   int m=0, n=0;
   for (unsigned int i = 0; i < dim; ++i)
@@ -111,13 +117,11 @@ int main(){
                       //applying symmetry along main diagonal (not automatically done for SymmetricTensor)
                       elasticityPrincipal[k][l][i][j] = elasticityPrincipal[i][j][k][l];
                     }
-                    //
                     else if(m==4 && n==4)
                     {
                       //C11 and C12 are already known based on order of for loops (all of i=1 is done first), but writing explicitly just to be safe
                       elasticityPrincipal[i][j][k][l] = (E1*(1-pow(nu23,2))-E2*nu12*(1+nu23))/(2*constk);
                     }
-                    //
                     else if((m==5 && n==5)||(m==6 && n==6))
                     {
                       elasticityPrincipal[i][j][k][l] = G12;
@@ -126,15 +130,6 @@ int main(){
               }
           }
       }
-
-  //creating array to get fiber coordinates before creating tensor
-  //dealii::Tensor<1, dim> fiber;
-  fiber[0] = 1;
-  fiber[1] = 2;
-  //define z axis only for 3D case (otherwise out of bounds)
-  if (dim == 3){
-    fiber[2] = 0;
-  }
   
   //Create projection of fiber onto xy plane to find angles
   //fiberxy will be the same as input fiber for 2d case, creating this way to avoid duplicate code for theta
@@ -142,21 +137,17 @@ int main(){
   fiberxy[0] = fiber[0];
   fiberxy[1] = fiber[1];
   //Create vector of x axis direction
-  dealii::Tensor<1, dim> xaxis;
+  dealii::Tensor<1, dim> xaxis ;
   xaxis[0] = 1;
-
-  //find theta from angle between x axis and xy projection
-  double theta = dealii::Physics::VectorRelations::angle(fiberxy, xaxis);
-  //TODO find how to prepopulate R, Rz and Ry from identity tensor using SymmetricTensor::unit_symmetric_tensor() function
-  //Prepopulating not really necessary, defaults to 0 value, so identity only ensures the "1" along main diagonal is filled for 3d
-  dealii::Tensor<2, dim> R;
-  dealii::Tensor<2, dim> Rz;
   
-  //temporary identity matrix function
-  for (int i = 0; i < dim; i++){
-    R[i][i] = 1;
-    Rz[i][i] = 1;
-  }
+  double theta = fiberxy.norm() == 0 ? 0 : dealii::Physics::VectorRelations::angle(fiberxy, xaxis);
+  // if(fiberxy.norm() != 0){
+  //   //find theta from angle between x axis and xy projection only if xy projection is non-zero
+  //   theta = dealii::Physics::VectorRelations::angle(fiberxy, xaxis);
+  // }
+  
+  dealii::Tensor<2, dim> R = unit_symmetric_tensor<dim>(), Rz = unit_symmetric_tensor<dim>();
+  //dealii::Tensor<2, dim> Rz = unit_symmetric_tensor<dim>();
 
   //Rotate about z axis, same process for both 2d and 3d 
   //TODO find way to condense this part?
@@ -175,15 +166,21 @@ int main(){
     
   }else if(dim == 3){
     //3d case needs phi to account for components in the z direction, which is found using xy projection and full fiber direction
-    //TODO finding phi and theta will cause issues for xy projection when fiber is purely in z direction
-    double phi = dealii::Physics::VectorRelations::angle(fiber, fiberxy);
+    const double pi = 3.14159265358979323846;
+    double phi = fiberxy.norm() == 0 ? pi/2 : dealii::Physics::VectorRelations::angle(fiber, fiberxy);
+    // double phi = dealii::Physics::VectorRelations::angle(fiber, fiberxy);
+    // if(fiberxy.norm() == 0){
+    //   //if fiberxy is 0, then vector is purely vertical, which is assumed to be 90 degrees
+    //   phi = pi/2;
+    // }else{
+    //   //3d case needs phi to account for components in the z direction, which is found using xy projection and full fiber direction
+    //   phi = dealii::Physics::VectorRelations::angle(fiber, fiberxy);
+    // }
+
     std::cout << "theta = " << 57.3*theta << "\nphi = " << 57.3*phi << "\n";
 
-    //same issues with Rz, identity tensor and condense assigning sin and cos values
-    dealii::Tensor<2, dim> Ry;
-    for (int i = 0; i < dim; i++){
-      Ry[i][i] = 1;
-    }
+    dealii::Tensor<2, dim> Ry = unit_symmetric_tensor<dim>();
+
     Ry[0][0] = cos(phi);
     Ry[0][2] = -sin(phi);
     Ry[2][0] = sin(phi);
@@ -195,17 +192,10 @@ int main(){
       << R[1][0] << " " << R[1][1] << " " << R[1][2] << "\n"
       << R[2][0] << " " << R[2][1] << " " << R[2][2] << "\n";
     */
-
-    //Testing rotation of x axis, which should produce the fiber direction, used for testing
-    /*dealii::Tensor<1, dim> ans = R*xaxis;
-    std::cout << ans[0] << "\n" << ans[1] << "\n" << ans[2] << "\n";
-    */
   }
   //Create temporary asymmetric tensor for multiplications then converting to symmetric after
   dealii::Tensor<4, dim> temp, temp2;
-  dealii::SymmetricTensor<4, dim> elasticityCartesian;
-  //TODO double check multiplication is correct
-  //https://stackoverflow.com/questions/50178156/efficient-tensor-multiplication
+  dealii::SymmetricTensor<4, dim> elasticityCartesian1, elasticityCartesian2;
 
   /*//Working rotation code
   temp = R*elasticityPrincipal;
@@ -296,19 +286,20 @@ int main(){
       for (unsigned int k = 0; k < dim; k++){
         for (unsigned int l = 0; l < dim; l++){
           temp[i][j][k][l] = temp2[j][i][l][k];
+          elasticityCartesian1[i][j][k][l] = temp2[j][i][l][k];
         }
       }
     }
   }
 
-
+  //if elasticityCartesian1 == elasticityCartesian2, then this loop can be removed, should be the same, but double check just in case
   //for loops required to move generic tensor object to symmetric object for output
   for (unsigned int i = 0; i < dim; i++){
     for (unsigned int j = 0; j < dim; j++){
       for (unsigned int k = 0; k < dim; k++){
         for (unsigned int l = 0; l < dim; l++){
           //for cases where ijkl=jikl=ijlk will be overwritten with the last entry, assumes it is already symmetric
-          elasticityCartesian[i][j][k][l] = temp[i][j][k][l];
+          elasticityCartesian2[i][j][k][l] = temp[i][j][k][l];
         }
       }
     }
@@ -316,23 +307,23 @@ int main(){
   
   //Using isotropic tensor code for testing matrix creation  
   //Pulling same vals of E and nu from anisotropic "isotropic" case
-  double E = E1;
-  double nu = nu12;
-  double lambda = E * nu / ((1 + nu) * (1 - 2 * nu));
-  double mu = E / (2 * (1 + nu));
-  dealii::SymmetricTensor<4, dim> elasticityIso;
-  for (unsigned int i = 0; i < dim; ++i){
-    for (unsigned int j = 0; j < dim; ++j){
-      for (unsigned int k = 0; k < dim; ++k){
-        for (unsigned int l = 0; l < dim; ++l){
-          elasticityIso[i][j][k][l] =
-            (i == k && j == l ? mu : 0.0) +
-            (i == l && j == k ? mu : 0.0) +
-            (i == j && k == l ? lambda : 0.0);
-        }
-      }
-    }
-  }
+  // double E = E1;
+  // double nu = nu12;
+  // double lambda = E * nu / ((1 + nu) * (1 - 2 * nu));
+  // double mu = E / (2 * (1 + nu));
+  // dealii::SymmetricTensor<4, dim> elasticityIso;
+  // for (unsigned int i = 0; i < dim; ++i){
+  //   for (unsigned int j = 0; j < dim; ++j){
+  //     for (unsigned int k = 0; k < dim; ++k){
+  //       for (unsigned int l = 0; l < dim; ++l){
+  //         elasticityIso[i][j][k][l] =
+  //           (i == k && j == l ? mu : 0.0) +
+  //           (i == l && j == k ? mu : 0.0) +
+  //           (i == j && k == l ? lambda : 0.0);
+  //       }
+  //     }
+  //   }
+  // }
 
   std::cout << std::scientific << std::setprecision(3);
 
@@ -344,20 +335,19 @@ int main(){
     filename = std::to_string((int)fiber[0]) + std::to_string((int)fiber[1]) + std::to_string((int)fiber[2]) + "_cpp.csv";
   }
   std::ofstream file(filename);
-  file << "Principal, Asymmetric Rotated, Symmetric Rotated \n";
+  file << "Principal, Asymmetric Rotated, Symmetric Rotated 1, Symmetric Rotated 2\n";
 
   //for loop to iterate through all values in elasticity tensors
   for (unsigned int i = 0; i < dim; i++){
     for (unsigned int j = 0; j < dim; j++){
       for (unsigned int k = 0; k < dim; k++){
         for (unsigned int l = 0; l < dim; l++){
-          //std::cout << elasticityIso[i][j][k][l] << "    " << elasticityPrincipal[i][j][k][l] << "    " << (int)temp[i][j][k][l] << "\n";
-          //file << elasticityPrincipal[i][j][k][l] << "," << (long)temp[i][j][k][l] << "," << (long)elasticityCartesian[i][j][k][l] << "\n";
-          file << elasticityPrincipal[i][j][k][l] << "," << temp[i][j][k][l] << "," << elasticityCartesian[i][j][k][l] << "\n";
+          //std::cout << (long)elasticityIso[i][j][k][l] << "    " << (long)elasticityPrincipal[i][j][k][l] << "    " << (long)temp[i][j][k][l] << "\n";
+          file << elasticityPrincipal[i][j][k][l] << "," << temp[i][j][k][l] << "," << elasticityCartesian1[i][j][k][l] << "," << elasticityCartesian2[i][j][k][l] << "\n";
         }
       }
     }
   }
-  std::cout << "Written to file " << filename << "successfully";
+  std::cout << "Written to file " << filename << " successfully \n";
     
 }
