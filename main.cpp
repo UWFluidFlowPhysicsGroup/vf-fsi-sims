7bb78cd3050824cc134f2c899d2ef38196d78e1d
nhewkoUW <nhewko@uwaterloo.ca>
2025-12-17 17:16:12 -0500

    Created branch for testing full vocal fold model

--- main.cpp
+++ main.cpp
@@ -74,8 +74,6 @@ int main(int argc, char *argv[]){
   // input mesh names for fluid and solid meshes here, using arrays to automate mesh refinement studies or other meshes as long as parameters match
   const std::string simMeshSolid[] = {"DualCollisionSolid"};
   const std::string simMeshFluid[] = {"DualCollisionFluid"};
-  // const std::string simMeshSolid[] = {"SquareMeshDualMat"};
-  // const std::string simMeshFluid[] = {""};
   const std::string meshPath = "meshes/";
   const std::string paramsPath = "parameters.prm";
   //read parameters file to determine the dimensions present
@@ -140,23 +138,23 @@ int main(int argc, char *argv[]){
             //combine solid and fluid meshes to make FSI simulation
             Solid::MPI::SharedLinearElasticity<2> solid(triaSolid, params);
             Fluid::MPI::SCnsIM<2> fluid(triaFluid, params);
-            
+            //bottom contact works, but not top for some reason?
             // Define penetration criterion, incompressible plane along mid plane
             auto penetration_criterion = [](const Point<2> &p, const Point<2> &disp) -> double {
-              // double midplane = (1.69-0.005)/2;
-              // double gap = 0.005;
-              double midplane = 0;
-              double gap = 0.02;
+              double midplane = 0.85;
+              double gap = 0.01;
               // Check if point is between min and max collision zone
               if ((p[1] >  (midplane - gap/2)) && (p[1] < (midplane + gap/2))){
                 if (disp[1] < 0){
                   return (midplane + gap/2 - p[1]);
-                }else if(disp[1] > 0){
+                }else{
                   return (p[1] - (midplane-gap/2));
                 }
+              //else probably not needed since above code returns value if collision occurs
+              }else{
+                //return value of 0 if no collision or if disp[1] = 0
+                return (0);
               }
-              //return value of 0 if no collision or if disp[1] = 0
-              return (0);
             };
 
             // keeping vertex point data for futureproofing mpi_fsi class, could also overload function instead
