87df469230aa501240887b4bc10c758ba075ea43
nhewkoUW <nhewko@uwaterloo.ca>
2026-03-17 11:23:30 -0400

    Reduced rectangular mesh height to 8.45mm, lowered contact plane to match and decreased stiffness to try to get sustained VF oscillation

--- main.cpp
+++ main.cpp
@@ -73,7 +73,7 @@ using namespace dealii;
 int main(int argc, char *argv[]){
   // input mesh names for fluid and solid meshes here, using arrays to automate mesh refinement studies or other meshes as long as parameters match
   const std::string simMeshSolid[] = {"M5_BC_Half"};
-  const std::string simMeshFluid[] = {"Fluid_Half_Gradient"};
+  const std::string simMeshFluid[] = {"Fluid_Half_100_Gradient"};
   const std::string meshPath = "meshes/";
   const std::string paramsPath = "parameters_2D_BC_Half.prm";
   //read parameters file to determine the dimensions present
@@ -141,8 +141,8 @@ int main(int argc, char *argv[]){
             
             // Define penetration criterion, incompressible plane along mid plane
             auto penetration_criterion = [](const Point<2> &p, const Point<2> &disp) -> double {
-              double midplane = 8.5;
-              double gap = 0.1;
+              double midplane = 8.45;
+              double gap = 0.025;
               // Check if point is between min and max collision zone
               if ((p[1] >  (midplane - gap/2)) && (p[1] < (midplane + gap/2))){
                 if (disp[1] < 0){
@@ -159,7 +159,7 @@ int main(int argc, char *argv[]){
               return (Tensor<1,2>({0,-disp[1]}));
             };
 
-            double PMLlength = 10, SigmaMax = 200000;
+            double PMLlength = 5, SigmaMax = 100000;
             auto sigma_pml_field =
             [PMLlength, SigmaMax](const Point<2> &p, const unsigned int component) {
               (void)component;
