83e58606dc4f269f575fda22c56ffa3a00779938
nhewkoUW <nhewko@uwaterloo.ca>
2026-05-20 14:20:00 -0400

    Expanded backward facing step region at outlet. Added PML regions to outlet zones and changed material parameters

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ using namespace dealii;
 int main(int argc, char *argv[]){
   // input mesh names for fluid and solid meshes here, using arrays to automate mesh refinement studies or other meshes as long as parameters match
   const std::string simMeshSolid[] = {"BC_Half_1mm"};
-  const std::string simMeshFluid[] = {"Fluid_Half"};
+  const std::string simMeshFluid[] = {"Fluid_Half_8.4"};
   const std::string meshPath = "meshes/";
   const std::string paramsPath = "parameters_2D_BC_Half.prm";
   //read parameters file to determine the dimensions present
@@ -131,6 +131,44 @@ int main(int argc, char *argv[]){
             solid.run();
           }else if(params.simulation_type == "Fluid"){
             Fluid::MPI::SCnsIM<2> fluid(triaFluid, params);
+            auto body_force = [](const Point<2> &point,
+                               const unsigned int component) -> double {
+              double bfMid = -50;
+              double bfLength = 10;
+              double bfMax = 0.75e3;
+              
+              if (std::abs(2*(point[0] - bfMid)) < bfLength && component == 0){
+                return bfMax * (pow(-2 * (point[0] - bfMax) / bfLength, 4) + 1);
+              }
+              return 0.0;
+            };
+
+            auto sigma_pml_field = [](const Point<2> &point,
+                                const unsigned int component) -> double {
+              (void)component;
+              double sigmaMax = 10e3;
+              double pmlLength = 10.0;
+              double sigmaPML = 0.0;
+              std::vector<double> boundary = {-50.0};
+              std::vector<unsigned int> boundary_dir = {0};
+              for (unsigned int i = 0; i < boundary.size(); ++i)
+                {
+                  if (std::abs(point[boundary_dir[i]] - boundary[i]) < pmlLength)
+                    {
+                      sigmaPML =
+                        sigmaMax *
+                        pow((pmlLength -
+                            std::abs(point[boundary_dir[i]] - boundary[i])) /
+                              pmlLength,
+                            4);
+                    }
+                }
+              return sigmaPML;
+            };
+
+            fluid.set_sigma_pml_field(sigma_pml_field);
+            fluid.set_body_force(body_force);
+            
             fluid.run();
           }else if(params.simulation_type == "FSI"){
             //combine solid and fluid meshes to make FSI simulation
@@ -160,8 +198,8 @@ int main(int argc, char *argv[]){
             auto body_force = [](const Point<2> &point,
                                const unsigned int component) -> double {
               double bfMid = -50;
-              double bfLength = 40;
-              double bfMax = 5000;
+              double bfLength = 10;
+              double bfMax = 0.75e3;
               
               if (std::abs(2*(point[0] - bfMid)) < bfLength && component == 0){
                 return bfMax * (pow(-2 * (point[0] - bfMax) / bfLength, 4) + 1);
@@ -175,19 +213,20 @@ int main(int argc, char *argv[]){
                                 const unsigned int component) -> double {
               (void)component;
               double sigmaMax = 10e3;
-              double pmlLength = 10.0;
               double sigmaPML = 0.0;
-              std::vector<double> boundary = {-50.0};
-              std::vector<unsigned int> boundary_dir = {0};
-              for (unsigned int i = 0; i < boundary.size(); ++i)
+              
+              std::vector<double> boundary = {-100.0,-70, 84.5};
+              std::vector<double> length = {30, 20, 20};
+              std::vector<unsigned int> boundary_dir = {0, 1, 0};
+              for (unsigned int i = 0; i < boundary.size(); i++)
                 {
-                  if (std::abs(point[boundary_dir[i]] - boundary[i]) < pmlLength)
+                  if (std::abs(point[boundary_dir[i]] - boundary[i]) < length[i])
                     {
                       sigmaPML =
                         sigmaMax *
-                        pow((pmlLength -
+                        pow((length[i] -
                             std::abs(point[boundary_dir[i]] - boundary[i])) /
-                              pmlLength,
+                              length[i],
                             4);
                     }
                 }
