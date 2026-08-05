d81e91c1066bea8e04263499012e960c80975cf1
Nicholas Hewko <nhewko@uwaterloo.ca>
2026-08-04 22:47:06 -0400

    Added fluid mesh with subdivided glottal region for better refinement, closer reflects reference simulations. Changed parameters to match reference simulations

--- main.cpp
+++ main.cpp
@@ -70,10 +70,10 @@ using namespace dealii;
 
 int main(int argc, char *argv[]){
   // input mesh names for fluid and solid meshes here, using arrays to automate mesh refinement studies or other meshes as long as parameters match
-  const std::string simMeshSolid[] = {"BCS_2_1"};
-  const std::string simMeshFluid[] = {"Fluid_Half"};
+  const std::string simMeshSolid[] = {"BC_Half"};
+  const std::string simMeshFluid[] = {"Fluid_Half_Subdivided"};
   const std::string meshPath = "meshes/";
-  const std::string paramsPath = "parameters_2D_BCS_Half.prm";
+  const std::string paramsPath = "parameters_2D_BC_Half.prm";
   //read parameters file to determine the dimensions present
   Parameters::AllParameters params(paramsPath);
   GridOut gridOut;
@@ -159,7 +159,7 @@ int main(int argc, char *argv[]){
 
             auto body_force = [](const Point<2> &point,
                                const unsigned int component) -> double {
-              double bfMid = -50;
+              double bfMid = -80;
               double bfLength = 10;
               double bfMax = 0.8e5*1e3;
 		            //Extra 1e3 fudge factor for unit conversions
@@ -174,10 +174,10 @@ int main(int argc, char *argv[]){
             auto sigma_pml_field = [](const Point<2> &point,
                                 const unsigned int component) -> double {
               (void)component;
-              double sigmaMax = 5e3;
+              double sigmaMax = 10e3;
               double sigmaPML = 0.0;
               
-              std::vector<double> boundary = {-120.0, -100, 290};
+              std::vector<double> boundary = {-125.0, -200, 390};
               std::vector<double> length = {30, 20, 20};
               std::vector<unsigned int> boundary_dir = {0, 1, 0};
               for (unsigned int i = 0; i < boundary.size(); i++)
