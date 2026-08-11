6019b60d75070202aa43ffb3559fc033408115f3
nhewkoUW <nhewko@uwaterloo.ca>
2026-08-11 16:01:04 -0400

    Updated inp to msh conversion script, which provided readable msh file

--- convert.py
+++ convert.py
@@ -3,3 +3,5 @@ import meshio
 mesh = meshio.read("vocal_tract.inp")
 
 meshio.write_points_cells("vocal_tract.msh", mesh.points, mesh.cells)
+
+# meshio convert --input-format gmsh --output-format abaqus inputfile.msh outputfile.inp
